2dc954f668893444167db7420940d50977bf1c01
cc32d9 <cc32d9@gmail.com>
2019-03-08 21:37:46 +0000

    First version on Jungle

---
+++ README.md
@@ -0,0 +1,109 @@
+# EOS API Directory smart contract
+
+This is an EOS smart contract that holds a catalog of various
+infrastructure APIs, such as history or account status APIs.
+
+The directory lists all known EOS networks. Network names and API types
+are added by directory administrators.
+
+Each API provider is identified by an account name. Administrators have
+to approve APi providers in order to avoid spamming.
+
+Each APi provider is allowed to specify one API URL of each type for
+each network. Apart from API URL, the provider needs to specify a
+two-letter ISO continent code and 2-letter country code. The code "ANY"
+can be used for both to indicate a georedundant API that is available
+from multiple continents.
+
+Valid continent codes: "AF", "AN", "AS", "EU", "NA", "OC", "SA".
+
+The API provider may modify or delete a corresponding entry in the
+directory.
+
+
+
+## Administrator actions
+
+```
+alias cl='cleos -v -u http://jungle2.cryptolions.io'
+
+cl set account permission apidirectory admin \
+ '{"threshold": 1,"keys": [],"accounts": [{"permission": {"actor": "cc32dninexxx","permission": "active"},"weight": 1}],"waits": []}' \
+ owner -p apidirectory@owner
+cl set action permission apidirectory apidirectory setnetwork admin
+cl set action permission apidirectory apidirectory setapitype admin
+cl set action permission apidirectory apidirectory approveprv admin
+
+
+cl push action apidirectory setnetwork '["eos", "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906", "EOS Mainnet", ""]' -p apidirectory@admin
+
+cl push action apidirectory setnetwork '["jungle", "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473", "EOS Jungle testnet", "https://jungletestnet.io/"]' -p apidirectory@admin
+
+cl push action apidirectory setnetwork '["kylin", "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191", "EOS Kylin testnet", "https://www.cryptokylin.io/"]' -p apidirectory@admin
+
+cl push action apidirectory setnetwork '["worbli", "73647cde120091e0a4b85bced2f3cfdb3041e266cbbe95cee59b73235a1b3b6f", "Worbli", "https://worbli.io/"]' -p apidirectory@admin
+
+cl push action apidirectory setnetwork '["bos", "d5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86", "BOSCORE", "https://boscore.io/"]' -p apidirectory@admin
+
+cl push action apidirectory setnetwork '["telos", "4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11", "Telos", "https://telosfoundation.io/"]' -p apidirectory@admin
+
+cl push action apidirectory setnetwork '["meet.one", "cfe6486a83bad4962f232d48003b1824ab5665c36778141034d75e57b956e422", "MEET.ONE", "https://meet.one/"]' -p apidirectory@admin
+
+cl push action apidirectory setnetwork '["eosforce", "bd61ae3a031e8ef2f97ee3b0e62776d6d30d4833c8f7c1645c657b149151004b", "EOS Force", "https://www.eosforce.io/"]' -p apidirectory@admin
+
+
+
+cl push action apidirectory setapitype '["v1chainpl", "chain_plugin in nodeos", "https://github.com/EOSIO/eos/tree/master/plugins/chain_plugin"]' -p apidirectory@admin
+
+cl push action apidirectory setapitype '["v1historypl", "Original history_plugin in nodeos", "https://github.com/EOSIO/eos/tree/master/plugins/history_plugin"]' -p apidirectory@admin
+
+cl push action apidirectory setapitype '["nodeosp2p", "net_plugin in nodeos", "https://github.com/EOSIO/eos/tree/master/plugins/net_plugin"]' -p apidirectory@admin
+
+cl push action apidirectory setapitype '["hyperion", "Scalable Full History API Solution", "https://github.com/eosrio/Hyperion-History-API"]' -p apidirectory@admin
+
+cl push action apidirectory setapitype '["lightapi", "EOS Light API", "https://github.com/cc32d9/eos_zmq_light_api"]' -p apidirectory@admin
+
+```
+
+
+# API Provider actions
+
+```
+alias cl='cleos -v -u http://jungle2.cryptolions.io'
+
+cl push action apidirectory setprovider '["cc32dninexxx", "https://github.com/cc32d9", "cc32d9@gmail.com", "telegram:cc32d9"]' -p cc32dninexxx@active
+
+## Administrator has to approve the new provider
+cl push action apidirectory approveprv '["cc32dninexxx"]' -p apidirectory@admin
+
+
+cl push action apidirectory updrec '["eos", "lightapi", "cc32dninexxx", "https://api.net.light.xeos.me", "ANY", "ANY"]' -p cc32dninexxx@active
+
+```
+
+# Accessing the records
+
+Use network name as scope in "records" table. The index #2 is by API type:
+
+```
+cl get table apidirectory eos records --index 2 --key-type name --lower lightapi
+
+```
+
+
+
+## Copyright and License
+
+Copyright 2019 cc32d9@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ apidirectory.cpp
@@ -0,0 +1,241 @@
+/*
+Copyright 2019 cc32d9@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/action.hpp>
+#include <eosiolib/multi_index.hpp>
+
+
+using namespace eosio;
+using std::string;
+
+const string iso_continents[] = {"ANY", "AF", "AN", "AS", "EU", "NA", "OC", "SA"};
+
+CONTRACT apidirectory : public eosio::contract {
+ public:
+  apidirectory( name self, name code, datastream<const char*> ds ):
+    contract(self, code, ds),
+    _networks(self, self.value),
+    _apitypes(self, self.value),
+    _providers(self, self.value)
+      {}
+
+  
+  ACTION setnetwork(name netname, checksum256  chainid, string description, string url)
+  {
+    require_auth(permission_level(_self, name("admin")));
+
+    auto setter = [&]( auto& item ) {
+      item.netname = netname;
+      item.chainid = chainid;
+      item.description = description;
+      item.url = url;
+    };
+    
+    auto netitr = _networks.find(netname.value);
+    if( netitr == _networks.end() ) {
+      _networks.emplace(_self, setter);
+    }
+    else {
+      _networks.modify(*netitr, _self, setter);
+    }
+  }
+
+
+  ACTION setapitype(name type, string description, string url)
+  {
+    require_auth(permission_level(_self, name("admin")));
+
+    auto setter = [&]( auto& item ) {
+      item.type = type;
+      item.description = description;
+      item.url = url;
+    };
+    
+    auto typitr = _apitypes.find(type.value);
+    if( typitr == _apitypes.end() ) {
+      _apitypes.emplace(_self, setter);
+    }
+    else {
+      _apitypes.modify(*typitr, _self, setter);
+    }
+  }
+  
+
+  ACTION setprovider(name provider, string url, string email, string im)
+  {
+    require_auth(provider);
+    
+    auto prvitr = _providers.find(provider.value);
+    if( prvitr == _providers.end() ) {
+      _providers.emplace(provider, [&]( auto& item ) {
+          item.provider = provider;
+          item.approved = false;
+          item.url = url;
+          item.email = email;
+          item.im = im;
+        });
+    }
+    else {
+      _providers.modify(*prvitr, provider, [&]( auto& item ) {
+          item.url = url;
+          item.email = email;
+          item.im = im;
+        });
+    }
+  }
+  
+  
+  ACTION approveprv(name provider)
+  {
+    require_auth(permission_level(_self, name("admin")));
+
+    auto prvitr = _providers.find(provider.value);
+    eosio_assert( prvitr != _providers.end(), "Cannot find provider");
+    _providers.modify(*prvitr, same_payer, [&]( auto& item ) {
+        item.approved = true;
+      });
+  }
+
+
+  ACTION updrec(name network, name type, name provider, string url,
+                string continent, string country)
+  {
+    require_auth(provider);
+    auto netitr = _networks.find(network.value);
+    eosio_assert( netitr != _networks.end(), "Invalid network name");
+    auto typitr = _apitypes.find(type.value);
+    eosio_assert(typitr != _apitypes.end(), "Invalid API type");
+    auto prvitr = _providers.find(provider.value);
+    eosio_assert( prvitr != _providers.end(), "Unknown provider name");
+    eosio_assert( prvitr->approved, "This provider is not yet approved");
+
+    bool found = false;
+    for(const string &cnt : iso_continents) {
+      if( continent == cnt ) {
+        found = true;
+        break;
+      }
+    }
+    eosio_assert(found, "Invalid continent name");
+
+    if( country != "ANY" ) {
+      eosio_assert(country.length() == 2, "ISO country code must be 2 letters");
+      for( int i = 0; i < country.length(); i++ ) {
+        char c = country[i];
+        eosio_assert('A' <= c && c <= 'Z', "Invalid character in ISO country code");
+      }
+    }
+
+    records _records(_self, network.value);
+    auto idx = _records.get_index<name("recidx")>();
+    auto recitr = idx.find(recidx(type, provider));
+    if( recitr == idx.end() ) {
+      _records.emplace(provider, [&]( auto& item ) {
+          item.id = _records.available_primary_key();
+          item.type = type;
+          item.provider = provider;
+          item.url = url;
+          item.continent = continent;
+          item.country = country;
+        });
+    }
+    else {
+      _records.modify(*recitr, provider, [&]( auto& item ) {
+          item.url = url;
+          item.continent = continent;
+          item.country = country;
+        });
+    }
+  }
+
+
+  ACTION delrec(name network, name type, name provider)
+  {
+    require_auth(provider);
+    records _records(_self, network.value);
+    auto idx = _records.get_index<name("recidx")>();
+    auto recitr = idx.find(recidx(type, provider));
+    eosio_assert(recitr != idx.end(), "Cannot find such record");
+    _records.erase(*recitr);
+  }
+      
+  
+ private:
+
+    struct [[eosio::table("networks")]] network {
+      name         netname;
+      checksum256  chainid;
+      string       description;
+      string       url;
+      auto primary_key()const { return netname.value; }
+    };
+
+    typedef eosio::multi_index<name("networks"), network> networks;
+    networks _networks;
+
+
+    struct [[eosio::table("apitypes")]] apitype {
+      name         type;
+      string       description;
+      string       url;
+      auto primary_key()const { return type.value; }
+    };
+
+    typedef eosio::multi_index<name("apitypes"), apitype> apitypes;
+    apitypes _apitypes;
+
+
+    struct [[eosio::table("providers")]] provider {
+      name         provider;
+      bool         approved;
+      string       url;
+      string       email;
+      string       im;
+      auto primary_key()const { return provider.value; }
+    };
+
+    typedef eosio::multi_index<name("providers"), provider> providers;
+    providers _providers;
+    
+
+    inline static uint128_t recidx(name type, name provider) {
+      return (((uint128_t)type.value)<<64) | provider.value;
+    }
+
+    // network name is the scope
+    struct [[eosio::table("records")]] record {
+      uint64_t     id;
+      name         type;
+      name         provider;
+      string       url;
+      string       continent;
+      string       country;
+      auto primary_key()const { return id; }
+      uint128_t get_recidx() const { return recidx(type, provider); }
+      uint64_t get_type() const { return type.value; }
+    };
+
+    typedef eosio::multi_index<name("records"), record,
+      indexed_by<name("type"), const_mem_fun<record, uint64_t, &record::get_type>>,
+      indexed_by<name("recidx"), const_mem_fun<record, uint128_t, &record::get_recidx>>
+    > records;
+    
+};
+
+
+EOSIO_DISPATCH(apidirectory, (setnetwork)(setapitype)(setprovider)(approveprv)(updrec)(delrec))
+
